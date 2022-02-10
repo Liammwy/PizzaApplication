ea3e994b4d7ccac5ccf714020a0652c94cef718e
liam <liam1148@live.co.uk>
2022-02-10 11:36:21 +0000

    A bunch of updates

--- PizzaApplication/MainWindow.xaml.cs
+++ PizzaApplication/MainWindow.xaml.cs
@@ -30,44 +30,58 @@ namespace PizzaApplication
             new ItemLists(this);
         }
 
+        //When the "Confirm Pizza" button is clicked, this method will run.
         private void ConfirmPizza_Click(object sender, RoutedEventArgs e)
         {
-            int Count = 0;
+            //A count integer will will increment based on how many of the sections are incorrect. This will be used to see if all sections have been selected or not.
+            bool valid = true;
             if (SelectPizza.SelectedItems.Count != 1)
             {
+                //"Trace" is just the same as Console, it will print out this message if the amount of pizzas selected is not 1.
                 Trace.WriteLine("No pizza has been selected.");
-                Count++;
+                valid = false;
             }
 
             if (SelectSize.SelectedItems.Count != 1)
             {
+                //This message will be printed out if the user hasn't selected a size.
                 Trace.WriteLine("Please enter a pizza size.");
-                Count++;
+                valid = false;
             }
 
             if (SelectTopping.SelectedItems.Count == 0)
             {
+                //This message will be printed out if the user hans't selected AT LEAST one topping (To allow for multiple).
                 Trace.WriteLine("Please select a topping.");
-                Count++;
+                valid = false;
             }
 
-            if (Count == 0)
+            //This will check to see if any of the fields are invalid. If they aren't, then the count will still be 0 and therefore this code runs.
+            if (valid == true)
             {
                 pizzaChoice.Pizza = SelectPizza.SelectedItem.ToString();
                 pizzaChoice.Size = SelectSize.SelectedItem.ToString();
                 pizzaChoice.Toppings = SelectTopping.SelectedItem.ToString();
+                //Creating a new list to store all toppings that have been selected, as this caused some issues before with adding them to the order list.
                 List<string> PizzaToppingsSelect = new List<string>();
+                //foreach statement to look for each topping that has been selected.
                 foreach(string topping in SelectTopping.SelectedItems)
                 {
+                    //If one of the toppings that have been selected is 'None'.
                     if (topping == "None")
                     {
+                        //Writing a line to console saying that the selection is none, just confirmation that the program is working as intended.
                         Trace.WriteLine("None topping selection has been made, therefore no pizzas will be added to the list.");
+                        //Clears the whole topping list so the other toppings selected won't be added.
                         PizzaToppingsSelect.Clear();
+                        //Add the "none" topping to the list so it's the only one that is displayed.
                         PizzaToppingsSelect.Add(topping);
+                        //Breaking the foreach loop so other items aren't added to the list afterwards.
                         break;
                     }
                     else
                     {
+                        //If the topping isn't "None" then the topping will be added to the list.
                         PizzaToppingsSelect.Add(topping);
                     }
                 }
@@ -85,6 +99,17 @@ namespace PizzaApplication
             SelectPizza.UnselectAll();
             SelectSize.UnselectAll();
             SelectTopping.UnselectAll();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (OrderList.SelectedIndex != -1)
+            {
+                for (int i = OrderList.SelectedItems.Count - 1; i >= 0; i--)
+                {
+                    OrderList.Items.Remove(OrderList.SelectedItems[i]);
+                }
+            }
         }
     }
 }
